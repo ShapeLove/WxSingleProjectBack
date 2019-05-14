955e1cf6ca1539b186d2b2a2ec93dcc89135d0d9
shp <1328445041@qq.com>
2019-05-14 08:45:07 +0800

    注释测试

--- src/main/java/com/shape/singleproject/filter/CorsFilter.java
+++ src/main/java/com/shape/singleproject/filter/CorsFilter.java
@@ -13,7 +13,8 @@ public class CorsFilter implements HandlerInterceptor{
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String origin = request.getHeader("Origin");
-        response.setHeader("Access-Control-Allow-Methods", origin);
+        response.setHeader("Access-Control-Allow-Origin", origin);
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Allow-Headers","Origin,Content-Type,Accept,token,X-Requested-With");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         return true;
